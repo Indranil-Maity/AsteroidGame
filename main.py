c894097e1d8b8dee0ee0113993f61479ab2354b5
Indranil Maity <indranilmaity1729@gmail.com>
2025-02-24 20:10:03 +0530

    added dt time

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pygame
 from constants import *
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
+    time = pygame.time.Clock()
+    dt = 0
     while running:
-        screen.fill("black")
-        pygame.display.flip()
-
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
+        
+        screen.fill("black")
+        pygame.display.flip()
+        dt = time.tick(60) / 1000
+
+        
 
 if __name__ == "__main__":
     main()
